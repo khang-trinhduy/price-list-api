3f620008a2383ce5e245d2873de4b8267d5ef165
Khang Duy <trinhduykhang1313@gmail.com>
2020-02-05 10:29:46 +0700

    refactor

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var provinceCtrl = require("../controllers/province");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "api resource" });
 });
 
 // price controller
