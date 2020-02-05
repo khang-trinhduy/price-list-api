3c97f70390459f5125c5bff1f42d174ab4a22632
Khang Duy <trinhduykhang1313@gmail.com>
2020-02-05 17:00:11 +0700

    update seed (again)

--- models/car/seed.js
+++ models/car/seed.js
@@ -7,26 +7,27 @@ exports.car_seed = [
     display: "VinFast FADIL Phiên Bản Tiêu Chuẩn"
   },
   {
-    name: "LUX A2.0",
-    version: "standard",
-    color: "",
-    price: "1099000000",
-    display: "VinFast LUX A 2.0 Phiên Bản Tiêu Chuẩn"
-  },
-  {
-    name: "LUX SA2.0",
-    version: "standard",
-    color: "",
-    price: "1530000000",
-    display: "VinFast LUX SA 2.0 Phiên Bản Tiêu Chuẩn"
-  },
-  {
     name: "FADIL",
     version: "advance",
     color: "",
     price: "429000000",
     display: "VinFast FADIL Phiên Bản Nâng Cao"
   },
+  {
+    name: "FADIL",
+    version: "luxury",
+    color: "",
+    price: "471900000",
+    display: "VinFast FADIL Phiên Bản Cao Cấp"
+  },
+  {
+    name: "LUX A2.0",
+    version: "standard",
+    color: "",
+    price: "1099000000",
+    display: "VinFast LUX A 2.0 Phiên Bản Tiêu Chuẩn"
+  },
+
   {
     name: "LUX A2.0",
     version: "advance",
@@ -42,6 +43,21 @@ exports.car_seed = [
     display: "VinFast LUX A 2.0 (Be/Nâu) Phiên Bản  Nâng Cao"
   },
   {
+    name: "LUX A2.0",
+    version: "luxury",
+    color: "Black",
+    price: "1337000000",
+    display: "VinFast LUX A 2.0 (Đen) Phiên Bản Cao Cấp"
+  },
+  {
+    name: "LUX SA2.0",
+    version: "standard",
+    color: "",
+    price: "1530000000",
+    display: "VinFast LUX SA 2.0 Phiên Bản Tiêu Chuẩn"
+  },
+
+  {
     name: "LUX SA2.0",
     version: "advance",
     color: "",
@@ -55,20 +71,7 @@ exports.car_seed = [
     price: "1814400000",
     display: "VinFast LUX SA 2.0 (Be/Nâu) Phiên Bản Nâng Cao"
   },
-  {
-    name: "FADIL",
-    version: "luxury",
-    color: "",
-    price: "471900000",
-    display: "VinFast FADIL Phiên Bản Cao Cấp"
-  },
-  {
-    name: "LUX A2.0",
-    version: "luxury",
-    color: "Black",
-    price: "1337000000",
-    display: "VinFast LUX A 2.0 (Đen) Phiên Bản Cao Cấp"
-  },
+
   {
     name: "LUX SA2.0",
     version: "luxury",
